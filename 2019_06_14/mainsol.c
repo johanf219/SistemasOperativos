c0fe65e02a594743d37e3c381a17f863534d3f67
johanf219 <florez.johan@correounivalle.edu.co>
2019-06-14 12:50:34 -0500

    agregando solución del taller

--- 2019_06_14/mainsol.c
+++ 2019_06_14/mainsol.c
@@ -1,13 +1,27 @@
+#include<unistd.h>
 #include "solucion.h"
 #include <stdio.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
+  pid_t pid;
+  int valorpadre;
   int x = 20;
 
-  printf("fibonacci(%d) = ",x);
-  x = fibonacci(x);
-  printf("%d\n",x);
-  guardarEntero("archivo",x);
+  pid = fork();
+  if(pid>0)
+  {
+    int valor=(int)leerEntero("archivo");
+    valorpadre = valor*100;
+    printf("El valor es:%d\n ",valorpadre);
+  }
+  else
+  {
+    printf("fibonacci(%d) = ",x);
+    x = fibonacci(x);
+    printf("%d\n",x);
+    guardarEntero("archivo",x);
 
-  return 0;
+    return 0;
+  }
 }
