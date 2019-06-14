996faefd3f64119ab417256aaa7bba57fd985358
johanf219 <florez.johan@correounivalle.edu.co>
2019-06-14 12:00:46 -0500

    adicionando fork-basico.c

--- 2019_06_14/fork-basico.c
+++ 2019_06_14/fork-basico.c
@@ -13,8 +13,13 @@ int main(int argc, char** argv)
   pid_t pid;
 
   pid= fork();
-  if(pid==0) 
-  { printf("Soy el hijo y mi identificador es:%d\n ",(int)getpid()); } // 0 -> hijo 
+  if(pid==0)
+  {
+    printf("Soy el hijo y mi identificador es:%d\n ",(int)getpid());
+   } // 0 -> hijo 
   else
-  { printf("Soy el padre y mi identificar es:%d\n y el de mi hijo es:%d\n ",(int)getppid(),(int)getpid()); }
+  {
+     printf("Soy el padre y mi identificar es:%d\n y el de mi hijo es:%d\n "
+     ,(int)getppid(),(int)getpid()); 
+  }
 }
